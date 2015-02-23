df88ad546911175f69871147144fea727624041b
nickpothier93 <nick.pothier93@gmail.com>
2015-02-22 22:11:59 -0400

    Correction sreen beta added

--- platforms/ios/www/js/game.js
+++ platforms/ios/www/js/game.js
@@ -13,6 +13,7 @@ var remaining = 120;
 var arrayofNumbers = [];
 var questionNumber = randomNumber();
 var lives = 3;
+var go = false;
 
 $(document).ready(function() {
     setQuestion(questions, questionNumber);
@@ -46,6 +47,16 @@ $(document).ready(function() {
 			}
 		window.location.href = "highscore.html";
 	});
+	$("#go").click(function(){
+		questionNumber = randomNumber();
+		setQuestion(questions, questionNumber);
+		go = false;
+		$("#right").css("display", "none");
+		$("#question").fadeIn(function(){
+			$("#questionCont").fadeIn(function(){
+			});
+		});	
+	});
 });
 
 function randomNumber(){
@@ -74,10 +85,27 @@ function randomNumber(){
 function setQuestion(questions, number){
 	console.log(questions.questions[number]);
 	$("#questionSpan").html(questions.questions[number].name);
-	$("#answerAtext").html("A: " +  questions.questions[number].a);
-	$("#answerBtext").html("B: " +  questions.questions[number].b);
-	$("#answerCtext").html("C: " +  questions.questions[number].c);
-	$("#answerDtext").html("D: " +  questions.questions[number].d);
+	if(questions.questions[number].amount == "2"){
+		$("#answerAtext").html("A: " +  questions.questions[number].a);
+		$("#answerBtext").html("B: " +  questions.questions[number].b);
+		$("#answerA").css("display", "block");
+		$("#answerB").css("display", "block");
+		$("#answerC").css("display", "none");
+		$("#answerD").css("display", "none");
+		$("#question").css("height", "270px");
+
+	}
+	else{
+		$("#answerAtext").html("A: " +  questions.questions[number].a);
+		$("#answerBtext").html("B: " +  questions.questions[number].b);
+		$("#answerCtext").html("C: " +  questions.questions[number].c);
+		$("#answerDtext").html("D: " +  questions.questions[number].d);
+		$("#answerA").css("display", "block");
+		$("#answerB").css("display", "block");
+		$("#answerC").css("display", "block");
+		$("#answerD").css("display", "block");
+		$("#question").css("height", "220px");
+	}
 }
 
 function questionCheck(){
@@ -93,13 +121,13 @@ function questionCheck(){
 				$("#lives").html("Lives : " + lives);
 				$("#answer" + answer.toUpperCase()).css("background-color", "red");
 				$("#answer" + questions.questions[questionNumber].right.toUpperCase()).css("background-color", "green");
+				go = true;
 			}
 			if(lives==0){
 				remaining = 0;
 			}
 			else{
 				answer = null;
-				questionNumber = randomNumber();
 				setTimeout(function() {
 					$("#questionCont").css("display", "none");
 					$("#question").css("display", "none");
@@ -108,11 +136,38 @@ function questionCheck(){
 					$("#answerB").css("background-color", "");
 					$("#answerC").css("background-color", "");
 					$("#answerD").css("background-color", "");
-					setQuestion(questions, questionNumber);
-					$("#question").fadeIn(function(){
-					    $("#questionCont").fadeIn(function(){
+					if(go == true){
+						$("#right").css("display", "block");
+						console.log(questions.questions[questionNumber].right);
+						if(questions.questions[questionNumber].right == "a"){
+							console.log(questions.questions[questionNumber].name);
+							$("#rightQuestion").html('The right answer for "<i>' + questions.questions[questionNumber].name +'</i>" is:');
+							$("#rightAnswer").html("<b>" + questions.questions[questionNumber].a + "</b>");
+						}
+						if(questions.questions[questionNumber].right == "b"){
+							console.log(questions.questions[questionNumber].name);
+							$("#rightQuestion").html('The right answer for "<i>' + questions.questions[questionNumber].name +'</i>" is:');
+							$("#rightAnswer").html("<b>" + questions.questions[questionNumber].b + "</b>");
+						}
+						if(questions.questions[questionNumber].right == "c"){
+							console.log(questions.questions[questionNumber].name);
+							$("#rightQuestion").html('The right answer for "<i>' + questions.questions[questionNumber].name +'</i>" is:');
+							$("#rightAnswer").html("<b>" + questions.questions[questionNumber].c + "</b>");
+						}
+						if(questions.questions[questionNumber].right == "d"){
+							console.log(questions.questions[questionNumber].name);
+							$("#rightQuestion").html('The right answer for "<i>' + questions.questions[questionNumber].name +'</i>" is:');
+							$("#rightAnswer").html("<b>" + questions.questions[questionNumber].d + "</b>");
+						}
+					}
+					else{
+						questionNumber = randomNumber();
+						setQuestion(questions, questionNumber);
+						$("#question").fadeIn(function(){
+					    	$("#questionCont").fadeIn(function(){
 				    	    });
-				    	});
+				    	});	
+					}
 				}, 500);
 			}
 		}
@@ -124,6 +179,7 @@ function time(){
 		$("#questionCont").css("display", "none");
 		$("#question").css("text-align", "center");
 		$("#question").html("GAME OVER");
+		$("#question").css("height", "220px");
 		$("#question").css("font-size", "26pt");
 		if(highscore() == true){
 			$("#highscoreCont").css("display", "block");
